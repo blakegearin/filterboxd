7651b394375a202536dd077fdba1ade76ca52368
Blake Gearin <hello@blakeg.me>
2025-02-08 01:50:52 -0700

    Release commit for 1.4.3

--- src/filterboxd.user.js
+++ src/filterboxd.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Filterboxd
 // @namespace    https://github.com/blakegearin/filterboxd
-// @version      1.4.2
+// @version      1.4.3
 // @description  Filter content on Letterboxd
 // @author       Blake Gearin <hello@blakeg.me> (https://blakegearin.com)
 // @match        https://letterboxd.com/*
